477d419a125ba3327436badac0c8b361eb45ed3b
plin11 <plin11@student.ubc.ca>
2025-02-20 16:53:05 -0800

    Added specification to MyRecipeApp class

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,5 +1,8 @@
 package ui;
 
+import model.*;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to my project!");
---
+++ src/main/ui/MyRecipeApp.java
@@ -0,0 +1,86 @@
+package ui;
+
+import model.*;
+import java.util.*;
+
+//Recipe manager application.
+public class MyRecipeApp {
+    private RecipeCollection recipeCollection;
+    private GroceryList groceryList;
+    private MealPlan mealPlan;
+    private Scanner input;
+
+    //Initializes the appplication.
+    private MyRecipeApp() {
+        // Stub
+    }
+
+    //MODIFIES: this
+    //EFFECTS: Runs the main application.
+    private void runMyRecipe() {
+        // Stub
+    }
+
+    //EFFECTS: Displays the main menu options to the user.
+    private void displayMenu() {
+        // Stub
+    }
+
+    //MODIFIES: recipeCollection
+    //EFFECTS: Conducts adding a new recipe.
+    private void addRecipe() {
+        // Stub
+    }
+
+    //MODIFIES: recipeCollection
+    //EFFECTS: Conducts deleting an existing recipe.
+    private void deleteRecipe() {
+        // Stub
+    }
+
+    //MODIFIES: groceryList
+    //EFFECT: Conducts adding a new item to the grocery list.
+    private void addItemToGroceryList() {
+        // Stub
+    } 
+
+    //MODIFIES: groceryList
+    //EFFECT: Conducts removing a specified item from the grocery list.
+    private void markItemAsBought() {
+        // Stub
+    } 
+
+    //MODIFIES: mealPlan
+    //EFFECTS: Conducts adding a meal in the meal plan.
+    private void addMeal() {
+        // Stub
+    }
+
+    //MODIFIES: mealPlan
+    //EFFECTS: Conducts deleting a specified meal in the meal plan.
+    private void deleteMeal() {
+        // Stub
+    }
+
+    //MODIFIES: 
+    //EFFECTS: Displays all recipes in the recipe collection.
+    private void viewRecipe() {
+        // Stub
+    }
+
+    //EFFECTS: Displays all items in the grocery list.
+    private void viewGroceryList() {
+        // Stub
+    }
+
+    //EFFECTS: Displays the current meal plan.
+    private void viewMealPlan() {
+        // Stub
+    }
+
+    //EFFECTS: Displays the meal plan of a specified day.
+    private void viewMealPlanOfSpecificDay() {
+        // Stub
+    }
+
+}
