628a8479a5a612ae1cdda1a0764301f869db98fe
plin11 <plin11@student.ubc.ca>
2025-02-28 16:42:57 -0800

    Added tests for JsonWriter clas

---
+++ src/main/test/persistence/JsonWriterTest.java
@@ -0,0 +1,24 @@
+package test.persistence;
+
+import model.*;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.File;
+
+class JsonWriterTest {
+    private static final String TEST_FILE = "./data/testRecipeData.json";
+
+    @Test
+    void testWriteRecipeCollection() {
+        RecipeCollection recipes = new RecipeCollection();
+        recipes.addRecipe(new Recipe("Spaghetti", "Italian"));
+
+        JsonWriter writer = new JsonWriter(TEST_FILE);
+        writer.write(recipes.toJson());
+
+        File file = new File(TEST_FILE);
+        assertTrue(file.exists());  
+    }
+}
