00f1612b3ba1cc9d31744ace02377b235d58dc51
plin11 <plin11@student.ubc.ca>
2025-02-28 17:20:15 -0800

    Updated tests for JsonWriter class

--- src/test/persistence/JsonWriterTest.java
+++ src/test/persistence/JsonWriterTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import java.io.File;
+import java.io.FileNotFoundException;
 
 class JsonWriterTest {
     private static final String TEST_FILE = "./data/testRecipeData.json";
@@ -16,10 +17,27 @@ class JsonWriterTest {
         RecipeCollection recipes = new RecipeCollection();
         recipes.addRecipe(new Recipe("Spaghetti", "Italian"));
 
-        JsonWriter writer = new JsonWriter(TEST_FILE);
-        writer.write(recipes.toJson());
+        GroceryList groceryList = new GroceryList();
+        groceryList.addItem("Tomatoes");
+        groceryList.addItem("Garlic");
 
-        File file = new File(TEST_FILE);
-        assertTrue(file.exists());  
+        MealPlan mealPlan = new MealPlan();
+        mealPlan.addMeal("Friday", new Recipe("Spaghetti", "Italian"));
+
+        JsonWriter writer = new JsonWriter(TEST_FILE);
+        
+        try {
+            writer.open();
+
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("recipes", recipes.toJson());
+            jsonObject.put("groceryList", groceryList.toJson());
+            jsonObject.put("mealPlan", mealPlan.toJson());
+
+            writer.write(jsonObject);
+            writer.close();
+        } catch (FileNotFoundException e) {
+            fail("File not found!");
+        }
     }
 }
