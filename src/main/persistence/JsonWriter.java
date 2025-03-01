4154e44e75276bb493131d217b00da3b642bfe93
plin11 <plin11@student.ubc.ca>
2025-02-28 16:33:20 -0800

    Added specification for JsonWriter class

---
+++ src/main/persistence/JsonWriter.java
@@ -0,0 +1,21 @@
+package persistence;
+
+import org.json.JSONObject;
+import java.io.FileWriter;
+import java.io.IOException;
+
+//Represents a writer that writes JSON representation of application data to a file.
+public class JsonWriter {
+    private String filePath;
+
+    //Construct writer with the specified file path.
+    public JsonWriter(String filePath) {
+        // Stub
+    }
+
+    //MODIFIES: this
+    // EFFECTS: writes the provided JSON object to file
+    public void write(JSONObject jsonObject) {
+        // Stub
+    }
+}
