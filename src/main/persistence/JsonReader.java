d50db5bd86c05c153413e65d299abae2468f5b96
plin11 <plin11@student.ubc.ca>
2025-02-28 17:20:42 -0800

    Added specification for JsonReader class

---
+++ src/main/persistence/JsonReader.java
@@ -0,0 +1,22 @@
+package persistence;
+
+import org.json.JSONObject;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class JsonReader {
+    private String source;
+
+    //EFFECTS: Creates reader to read from the source file.
+    public JsonReader(String source) {
+        // Stub
+    }
+
+    //MODIFIES: this
+    //EFFECTS: Reads JSON data from the file and return in the form of JSON object.
+    public JSONObject read() throws IOException {
+        // Stub
+        return null;
+    }
+}
